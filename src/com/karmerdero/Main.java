95e5da58ded5dfd44216f1b64b4925a7fed8323c
Kateryna-Chernovol <schasliva@yahoo.com>
2021-09-22 10:09:37 +0300

    As initial, but formated

--- src/com/karmerdero/Main.java
+++ src/com/karmerdero/Main.java
@@ -7,21 +7,21 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println();
-        Integer [] array = {2,3,1,7,11};
+        Integer[] array = {2, 3, 1, 7, 11};
         System.out.println
                 ("Array before sorting: "
                         + "\n"
-                        +  Arrays.toString(array));
+                        + Arrays.toString(array));
         Arrays.sort(array, Collections.reverseOrder());
         System.out.println
                 ("Array sorted in descending order: "
                         + "\n"
-                        +  Arrays.toString(array) + "\n");
+                        + Arrays.toString(array) + "\n");
 
         int sumPositiveValues = 0;
-        Integer []otherArray = {-15,3,18,5,12,-3,-10,1};
-        for (int i = 0; i < otherArray.length; i++){
-            if (otherArray[i] > 0){
+        Integer[] otherArray = {-15, 3, 18, 5, 12, -3, -10, 1};
+        for (int i = 0; i < otherArray.length; i++) {
+            if (otherArray[i] > 0) {
                 sumPositiveValues += otherArray[i];
             }
         }
@@ -30,20 +30,20 @@ public class Main {
 
         double avarage = 0;
         int count = 0;
-        for (int i = 0; i < otherArray.length; i++){
+        for (int i = 0; i < otherArray.length; i++) {
             avarage += otherArray[i];
-            count ++;
+            count++;
         }
         avarage = avarage / count;
         System.out.println("Avavage of all values: " + avarage + "\n");
 
-        int [] thirdArray = {3,2,3,1,4,2,8,3};
+        int[] thirdArray = {3, 2, 3, 1, 4, 2, 8, 3};
         System.out.println("The third array: " + Arrays.toString(thirdArray));
-        for (int i = 0; i <thirdArray.length; i++){
-            for (int j = 0; j < thirdArray.length; j++){
+        for (int i = 0; i < thirdArray.length; i++) {
+            for (int j = 0; j < thirdArray.length; j++) {
                 if (i == j) {
                     continue;
-                } else if (thirdArray[i] == thirdArray[j]){
+                } else if (thirdArray[i] == thirdArray[j]) {
                     thirdArray[j] = 0;
                 }
             }
@@ -51,9 +51,5 @@ public class Main {
         System.out.println("Replace all duplicated values by 0: ");
         System.out.println(Arrays.toString(thirdArray));
         System.out.println();
-//        System.out.println("Why arrays in java initialize with fixed size and can’t be dynamic?");
-//        System.out.println("Array memory is allocated on creation. " +
-//                "\n"+"When array initialized, it allocates the " +
-//                "\n"+"memory according to the it's size and type. ");
     }
 }
